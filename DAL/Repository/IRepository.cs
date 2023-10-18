3e1cc6b6ede1ccf0292ea2c4f7591dca84ea3cb6
Swaki123 <william.a.seger@gmail.com>
2023-10-18 13:21:45 +0200

    Added models and repository pattern

---
+++ DAL/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public interface IRepository<T>
+    {
+        List<T> getAll();
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void SaveChanges();
+    }
+}
---
+++ Models/Flow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Flow
+    {
+        private string Name { get; set; }
+        private string Title { get; set; }
+        private Category Category { get; set; }
+        private List<Episode> Episodes { get; set; }
+        public Flow(string name, string title, Category category, List<Episode> episodes)
+        {
+            Name = name;
+            Title = title;
+            Category = category;
+            Episodes = episodes;
+        }
+
+
+    }
+}
