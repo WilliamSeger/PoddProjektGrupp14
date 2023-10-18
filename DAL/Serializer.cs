bca40cea375570cad4d136bf565ec8c494ad896b
Swaki123 <william.a.seger@gmail.com>
2023-10-18 12:41:33 +0200

    XML serializer

---
+++ DAL/Serializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DAL
+{
+    internal class Serializer<T>
+    {
+        public Serializer() { }
+
+        public void Serialize(string filepath, List<T> dataList)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<T>));
+            using (FileStream xmlStream = new FileStream(filepath, FileMode.Create, FileAccess.Write)) 
+            {
+                xmlSerializer.Serialize(xmlStream, dataList);
+            }
+        }
+    }
+}
