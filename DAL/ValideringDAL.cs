e3ba3c4e25f51556cde9ab357d654f243ea19dbd
HUSCO96 <husco02@gmail.com>
2023-10-27 12:50:18 +0200

    Fixed validationclass and put it in PL layer

---
+++ DAL/ValideringDAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DAL
+{
+    public class ValideringDAL
+    {
+        public bool ValidateXmlFile(string filePath)
+        {
+            try
+            {
+
+                using (XmlReader reader = XmlReader.Create(filePath))
+                {
+
+                    while (reader.Read()) { }
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fel vid validering av XML-filen: " + ex.Message, "Fel!");
+                return false;
+            }
+        }
+    }
+}
