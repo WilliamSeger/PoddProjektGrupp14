32e46df7e8593052356b283b68e007f44cd3d129
Swaki123 <william.a.seger@gmail.com>
2023-10-30 10:21:18 +0100

    Removed unnecessary using-statements

--- BLL/Controllers/FlowController.cs
+++ BLL/Controllers/FlowController.cs
@@ -2,11 +2,6 @@
 using DAL;
 using Models;
 using System.ServiceModel.Syndication;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL.Controllers
 {
--- DAL/Repository/FlowRepository.cs
+++ DAL/Repository/FlowRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Models;
-using System.ServiceModel.Syndication;
+﻿using Models;
 
 namespace DAL.Repository
 {
